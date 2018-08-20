e21455cd2f37ffc435c37d061b07218c1be7df46
xiejj <v-xiejj05@vanke.com>
2018-08-20 18:31:50 +0800

    addreadeMD

---
+++ READE.md
@@ -0,0 +1,113 @@
+# 简介
+
+Weex-request 是封装weex中的stream.fetch模块，实现了便捷方法和拦截请求，可以像axios一样使用。
+
+# Example
+
+Performing a `GET` request
+
+```javascript
+import weexRequest from "weex-request";
+
+// Make a request for a user with a given ID
+weexRequest.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+```
+
+
+
+weexRequest(config)
+
+```javascript
+// Send a POST request
+weexRequest({
+  method: 'post',
+  url: '/user/12345',
+  data: {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  }
+});
+
+// GET request for remote image
+weexRequest({
+  method:'get',
+  url:'http://bit.ly/2mTM3nY',
+  type:'stream'
+})
+  .then(function(response) {
+    response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+  });
+```
+
+### Request method aliases
+
+For convenience aliases have been provided for all supported request methods.
+
+##### weexRequest.request(config)
+
+##### weexRequest.get(url[, config])
+
+##### weexRequest.delete(url[, config])
+
+##### weexRequest.head(url[, config])
+
+##### weexRequest.options(url[, config])
+
+##### weexRequest.post(url[, data[, config]])
+
+##### weexRequest.put(url[, data[, config]])
+
+##### weexRequest.patch(url[, data[, config]])
+
+
+
+## Global weexRequest defaults
+
+```javascript
+// 设置默认选项
+weexRequest.default.baseUrl = "https://api.github.com/";
+weexRequest.default.headers = {
+    "Content-Type": "application/json"
+};
+```
+
+
+
+## Interceptors
+
+You can intercept requests or responses before they are handled by `then` or `catch`.
+
+
+```javascript
+import weexRequest from "weex-request";
+
+// 设置默认选项
+weexRequest.default.baseUrl = "https://api.github.com/";
+weexRequest.default.headers = {
+    "Content-Type": "application/json"
+};
+// 拦截请求
+weexRequest.interceptors.request = function(config) {
+    console.log("config", config);
+    return Promise.resolve(config);
+};
+
+// 拦截响应
+weexRequest.interceptors.response = function(response) {
+    console.log("response", response);
+    return Promise.resolve(response);
+};
+
+export default weexRequest;
+```
--- index.js
+++ index.js
@@ -1,129 +1,22 @@
-const stream = weex.requireModule("stream");
+const Fetcher = require("./lib/fetch");
 
-class Fetcher {
-    constructor() {
-        this.default = {};
-        this.init();
-    }
-    init() {
-        // 初始化
-        this.default.baseUrl = "";
-        this.default.headers = {};
-        this.interceptors = {
-            request: null,
-            response: null
-        };
-    }
-    _fetch(opt) {
-        // 调用weex fetch发请求
-        return new Promise((resolve, reject) => {
-            // 转成Promise
-            stream.fetch(opt, response => {
-                if (!response.ok) {
-                    reject(response);
-                    return;
-                }
-                resolve(response);
-            });
-        });
-    }
-    async _proxy(opt) {
-        // 合并参数
-        opt = this._params(opt);
-        try {
-            // 拦截请求
-            if (
-                this.interceptors.request &&
-                typeof this.interceptors.request == "function"
-            ) {
-                opt = await this.interceptors.request(opt);
-            }
-            const res = await this._fetch(opt);
-            // 拦截响应
-            if (
-                this.interceptors.response &&
-                typeof this.interceptors.response == "function"
-            ) {
-                return this.interceptors.response(res);
-            }
-            // 正常返回
-            return Promise.resolve(res);
-        } catch (error) {
-            return Promise.reject(error);
-        }
-    }
-    _params(opt) {
-        opt.method = opt.method || "GET";
-        opt.url = opt.url || "";
-        opt.headers = opt.headers || {};
-        opt.type = opt.type || "json";
-        // 合并请求头
-        Object.assign(opt.headers, this.default.headers);
+const fetcher = new Fetcher();
 
-        // 合并url
-        if (!(opt.url && opt.url.startsWith("http"))) {
-            if (opt.url.startsWith("/")) {
-                opt.url = opt.url.replace("/", "");
-            }
-            if (this.default.baseUrl && !this.default.baseUrl.endsWith("/")) {
-                this.default.baseUrl = this.default.baseUrl + "/";
-            }
-            opt.url = this.default.baseUrl + opt.url;
-        }
-        // 转换请求体
-        if (opt.body && typeof opt.body == "object") {
-            for (const key in opt.body) {
-                opt.body[key] = opt.body[key] + "";
-            }
-        }
-        // 默认类型
-        opt.type = opt.type || "json";
-        return opt;
-    }
-    get(url = "", config = {}) {
-        const _opt = {
-            method: "GET",
-            url
-        };
-        Object.assign(_opt, config);
-        return this._proxy(_opt);
-    }
-    post(url = "", data, config = {}) {
-        const _opt = {
-            method: "POST",
-            url,
-            body: data
-        };
-        Object.assign(_opt, config);
-        return this._proxy(_opt);
-    }
-    put(url = "", data, config = {}) {
-        const _opt = {
-            method: "PUT",
-            url,
-            body: data
-        };
-        Object.assign(_opt, config);
-        return this._proxy(_opt);
-    }
-    delete(url = "", data, config = {}) {
-        const _opt = {
-            method: "DELETE",
-            url,
-            body: data
-        };
-        Object.assign(_opt, config);
-        return this._proxy(_opt);
-    }
-    delete(url = "", data, config = {}) {
-        const _opt = {
-            method: "PATCH",
-            url,
-            body: data
-        };
-        Object.assign(_opt, config);
-        return this._proxy(_opt);
-    }
-}
+const weexRequest = fetcher._proxy;
 
-module.exports = new Fetcher();
+// 全局设置
+weexRequest.default = fetcher.default;
+// 拦截请求
+weexRequest.interceptors = fetcher.interceptors;
+// get 方法
+weexRequest.get = fetcher.get;
+// post 方法
+weexRequest.post = fetcher.post;
+// put 方法
+weexRequest.put = fetcher.put;
+// delete 方法
+weexRequest.delete = fetcher.delete;
+// patch 方法
+weexRequest.patch = fetcher.patch;
+
+module.exports = weexRequest;
